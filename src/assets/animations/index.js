d5592664b941f13a48f35dde15a670c258971113
twogog <twogogtwogog@gmail.com>
2023-06-02 02:12:49 +0600

    fix import

--- src/assets/animations/index.js
+++ src/assets/animations/index.js
@@ -1,7 +1,7 @@
 import { wavyCirle } from './WavyCircle.js'
 import { futuristicButton } from './FuturisticButton.js'
 import { quickTextAnimation } from './QuickTextAnimation.js'
-import { dinoGame } from './Dinogame.js'
+import { dinoGame } from './DinoGame.js'
 
 export const animations = [null, wavyCirle, futuristicButton,
   quickTextAnimation, dinoGame];
