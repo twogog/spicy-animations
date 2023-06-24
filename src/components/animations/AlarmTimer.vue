bc2e5a6869ba04f680fb97a136287b31adab6da4
twogog <twogogtwogog@gmail.com>
2023-06-25 03:10:02 +0600

    circle progress in timer

--- src/components/animations/AlarmTimer.vue
+++ src/components/animations/AlarmTimer.vue
@@ -4,32 +4,54 @@ import { ref, reactive } from 'vue'
 const showTimer = ref(false)
 const showSettings = ref(true)
 const minutesInput = ref(null)
-const timer = ref(null)
+
+let timer = null
+
+const progressBar = ref(null)
 
 const hms = reactive({
   hours: null,
   minutes: null,
-  seconds: null
+  seconds: null,
+  purpose: null
 })
 
 function startTimer() {
+  const ctx = progressBar.value.getContext('2d')
+  ctx.strokeStyle = 'green'
+  ctx.lineWidth = 10
+  const canvasWidth = progressBar.value.width
+  const canvasHeight = progressBar.value.height
+
+  hms.purpose = minutesInput.value * 60000 // hours, minutes, seconds
+  let step = 360 / (minutesInput.value * 60)
+  let angle = 270 - step
+
   showTimer.value = true
   showSettings.value = false
-  const endDate = new Date()
-  const utcOffset = new Date().getTimezoneOffset()
-  endDate.setMinutes(endDate.getMinutes() + minutesInput.value)
 
-  timer.value = setInterval(() => {
-    const currentDate = new Date()
-    const result = new Date(
-      Math.floor(endDate / 1000) * 1000 - Math.floor(currentDate / 1000) * 1000
-    )
+  timer = setInterval(() => {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+    ctx.beginPath()
+    ctx.arc(canvasWidth / 2, canvasHeight / 2, 80, toRadians(270), toRadians((angle += step)))
+    ctx.stroke()
 
-    hms.hours = result.getHours() + utcOffset / 60
-    hms.minutes = result.getMinutes()
-    hms.seconds = result.getSeconds()
-    if (hms.hours === 0 && hms.minutes === 0 && hms.seconds === 0) clearInterval(timer.value)
+    timerRush(hms, timer)
   }, 1000)
+}
+
+function timerRush(hms, timer) {
+  if (hms.purpose === 0) clearInterval(timer)
+  const currentDate = new Date(hms.purpose)
+  hms.purpose -= 1000
+  hms.hours = currentDate.getUTCHours()
+  hms.minutes = currentDate.getMinutes()
+  hms.seconds = currentDate.getSeconds()
+}
+
+function toRadians(degrees) {
+  const pi = Math.PI
+  return degrees * (pi / 180)
 }
 </script>
 
@@ -47,7 +69,8 @@ function startTimer() {
     />
   </form>
   <div v-show="showTimer" class="timer">
-    <time v-if="hms.hours !== null">{{
+    <canvas ref="progressBar" width="300" height="250"></canvas>
+    <time v-if="hms.hours !== null" ref="timerFrame">{{
       `${hms.hours.toString().padStart(2, 0)}:${hms.minutes.toString().padStart(2, 0)}:${hms.seconds
         .toString()
         .padStart(2, 0)}`
@@ -69,6 +92,7 @@ function startTimer() {
   text-align: center;
 }
 .timer {
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -76,5 +100,8 @@ function startTimer() {
 .timer time {
   font-size: 3rem;
   color: white;
+}
+canvas {
+  position: absolute;
 }
 </style>
