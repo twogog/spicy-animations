168a5acc7de93c02d11f2b6476eadf57f1c43794
twogog <you@example.com>
2023-06-27 00:42:26 +0600

    fix synth on mobile

--- src/components/animations/AlarmTimer.vue
+++ src/components/animations/AlarmTimer.vue
@@ -49,7 +49,7 @@ function resetHMS(hms) {
 }
 
 function startTimer(e, ifpaused) {
-  new SpeechSynthesisUtterance('')
+  const speaker = new SpeechSynthesisUtterance(phrase.value) // needs for mobile activation
   if (!ifpaused) hms.purpose = secondsInput.value * 1000 // hours, minutes, seconds
   let step = 360 / secondsInput.value
   if (!hms.angle) hms.angle = 270 - step
@@ -59,7 +59,7 @@ function startTimer(e, ifpaused) {
 
   timer = setInterval(() => {
     draw(step)
-    timerRush(hms, timer)
+    timerRush(hms, timer, speaker)
   }, 1000)
 }
 
@@ -76,12 +76,14 @@ function draw(step) {
   canvas.ctx.stroke()
 }
 
-function timerRush(hms, timer) {
+function timerRush(hms, timer, speaker) {
   const currentDate = new Date(hms.purpose)
   if (hms.purpose === 0) {
     hms.seconds = currentDate.getSeconds()
     clearInterval(timer)
-    talkPhrase(phrase.value)
+    
+    configPhrase(speaker, phrase.value)
+    speechSynthesis.speak(speaker)
     return
   }
   hms.purpose -= 1000
@@ -114,8 +116,7 @@ function pauseTimer() {
   }
 }
 
-function talkPhrase(phrase) {
-  const speaker = new SpeechSynthesisUtterance(phrase)
+function configPhrase(speaker, phrase) {
   if (/[a-zA-Z]/.test(phrase)) {
     speaker.voice = voices.filter((v) => v.lang.includes('en'))[0]
     speaker.lang = 'en-US';
@@ -125,8 +126,6 @@ function talkPhrase(phrase) {
     speaker.voice = choose
     speaker.lang = 'ru-RU'
   }
-
-  speechSynthesis.speak(speaker)
 }
 </script>
 
